a6a08b8bfe8d48e69e20b500badead627984ab42
Franklin de Chanaud <franklin@lecloset.fr>
2020-11-16 16:36:41 +0100

    set examples for demo

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -1,18 +1,40 @@
-// Visit The Stimulus Handbook for more details 
-// https://stimulusjs.org/handbook/introduction
-// 
-// This example controller works with specially annotated HTML like:
-//
-// <div data-controller="hello">
-//   <h1 data-target="hello.output"></h1>
-// </div>
-
 import { Controller } from "stimulus"
 
-export default class extends Controller {
-  static targets = [ "output" ]
+// export default class extends Controller {
+//   initialize() {
+//     console.log('initialize!')
+//   }
+  
+//   connect() {
+//     console.log('connect!')
+//   }
+//   disconnect() {
+//     console.log('disconnect')
+//   }
+// }
 
-  connect() {
-    this.outputTarget.textContent = 'Hello, Stimulus!'
-  }
-}
+
+// export default class extends Controller {
+//   greet(e) {
+//     console.log('Event:', e);
+//     console.log('Greet!');
+//   }
+// }
+
+// export default class extends Controller {
+//   static targets = [ "name", "output" ]
+
+//   greet() {
+//     this.outputTarget.textContent =
+//       `Hello, ${this.nameTarget.value}!`
+//   }
+// }
+
+
+// export default class extends Controller {
+//   static targets = [ "name", "output" ]
+
+//   greet() {
+//     this.outputTargets.forEach((t) => t.textContent = `Hello, ${this.nameTarget.value}!`);
+//   }
+// }
